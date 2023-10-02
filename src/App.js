df17b210425ff4876c8d5fc5ba80b41cbeb8eeff
Sudipto Das <98449874+Sudip200@users.noreply.github.com>
2023-10-02 06:51:07 +0000

    enhanching ui

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const chats=[
     name:"",
     cat:"", 
     logo:'',
-    timestamp:'12:00',
+    timestamp:'12:00 AM',
     seen:true
 
   },
@@ -25,7 +25,7 @@ const chats=[
     sender:'user',
     name:"Jav",
     cat:"Engineering", 
-    logo:'',
+    logo:'https://lh3.googleusercontent.com/a/ACg8ocKI1QhjTNVog0soXa0z_Sq6qpVqparopRH1v4n-QWeMmDc=s432-c-no',
     timestamp:'12:04',
     seen:false
   }
@@ -70,7 +70,7 @@ function Content() {
      {
         chats.map((chat)=>{
           return(
-            <Chat content={chat.content} timestamp={chat.timestamp} seen={chat.seen} sender={chat.sender} />
+            <Chat content={chat.content} timestamp={chat.timestamp} seen={chat.seen} sender={chat.sender} logo={chat.logo}/>
           )
         })
      }
@@ -80,14 +80,21 @@ function Content() {
 function Chat(props) {
   return (
     <div className="Chat" style={{justifyContent:props.sender==="me"?"flex-end":"flex-start"}}>
+      {props.sender==="user" && <div className='chat-logo'>
+        <img src={props.logo} className="logo" alt="logo" />
+        </div>}
       <div className="chat-container" style={{backgroundColor:props.sender==="me"?"hsla(211, 100%, 50%, 1)":"hsla(240, 24%, 96%, 1)",marginLeft:props.sender==="me"?"0px":"20px",marginRight:props.sender==="me"?"20px":"0px"}}>
         <div className="chat-content">
           <div className="chat-content-text" style={{color:props.sender==="me"?"white":"black"}}>
             {props.content}
           </div>
-          <div className="chat-content-time">
+          <div className="chat-content-time" style={{color:props.sender==="me"?"white":"black"}}>
             {props.timestamp}
+            {props.sender ==="me" && <div className='chat-seen'>
+            <img src='seen.png' className="seen-logo" alt="logo"  width="20px" />
+           </div>}
           </div>
+           
         </div>
       </div>
     </div>
